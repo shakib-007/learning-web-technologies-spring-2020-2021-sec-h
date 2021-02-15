7f95a353cced6a6ab7e71986e22759bcbe133ed7
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-02-15 23:08:06 +0600

    task_1

---
+++ MID_LAB_TASK_PHP_04/AreaPerimeter.php
@@ -0,0 +1,11 @@
+<?php
+	$length=20;
+	$width=10;
+
+	$area=$length*$width;
+	$perimeter=2*($length+$width);
+	
+	print("The area of Rectangle = ".$area."\n");
+
+	print("The perimeter of Rectangle = ".$perimeter);
+?>
