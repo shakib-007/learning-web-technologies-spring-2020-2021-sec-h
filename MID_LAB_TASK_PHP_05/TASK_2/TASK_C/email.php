2ce3cc5846589e7ee9e3481f0135fc85bf8ac41f
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-02-16 15:13:28 +0600

    task C

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_C/email.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email input</title>
+</head>
+<body>
+
+	<form method="POST" action="">
+		<fieldset>
+			<legend>EMAIL</legend>
+			Name: <input type="email" name="email" value="<?php if(isset($_POST['email'])){ echo $_POST['email']; }?> ">  <br>			
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
