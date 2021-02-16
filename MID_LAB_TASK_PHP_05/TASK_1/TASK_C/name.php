9e2ac4e762367277716c869fce2feca280e7f591
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-02-16 13:47:40 +0600

    task c

---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_C/name.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name input</title>
+</head>
+<body>
+
+	<form method="POST" action="">
+		<fieldset>
+			<legend>NAME</legend>
+			Name: <input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname']; }?> ">  <br>			
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
