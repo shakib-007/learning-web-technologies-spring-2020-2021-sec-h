969f57a0bdcb4f04de135d8550c39376e8e470c9
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-03-14 12:29:44 +0600

    1st commit at 12:30

---
+++ MID_LAB_EXAM/registration.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<form method="POST" action="../controller/regCheck.php">
+        <fieldset style="width: 30%">
+              <legend>REGISTRATION</legend>
+              <table>
+                <tr>
+                  <td>Id</td>
+                  <td>:<input type="text" name="id" value="">  </td>
+                </tr>
+                
+                 
+                <tr>
+                  <td>Password</td>     
+                  <td>:<input type="password" name="password" value=""> </td>
+                </tr>
+               
+                <tr>
+                  <td>Confirm Password</td>
+                  <td>:<input type="password" name="conpass" value=""> </td>
+                </tr>
+                <tr>
+                  <td>Name</td>
+                  <td>:<input type="text" name="name" value="">  </td>
+                </tr>
+                
+           </table>
+            <p>User Type</p>
+            <hr>
+                  <input type="radio" name="gender"> User
+                  <input type="radio" name="gender"> Admin
+                
+                  
+            
+            <br>
+          
+              <input type="submit" name="submit" value="Signup">
+              <a href="login.php">Sign In</a>
+        
+         
+        </form>
+</body>
+</html>
