9bd58436650232984073e344c1a8a996e5004ec0
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-03-20 15:33:02 +0600

    final lab 1

---
+++ Final_Lab_Task_01_DB/controller/regCheck.php
@@ -0,0 +1,36 @@
+<?php
+	require('../model/db.php');
+	session_start();
+
+	if(isset($_POST['signup'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$repass = $_POST['repass'];
+		$email = $_POST['email'];
+		$sql = "insert into user values('1', '$username', '$password', '$email')";
+
+		if($username == "" || $email == "" || $password == "" || $repass == ""){
+			echo "null submission...";
+		}else{
+
+			if($password == $repass){
+
+				$user = [	
+							'username'=>$username, 
+							'password'=>$password, 
+							'email'=> $email
+						];
+
+				//$_SESSION['username'] = $username;
+				//$_SESSION['password'] = $password;
+				$_SESSION['current_user'] = $user;
+
+				header('location: ../view/login.html');
+			}else{
+				echo "password & confirm password mismatch..";
+			}
+		}
+
+	}
+?>
