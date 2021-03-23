cfbeab584354c4c3f849ea0d1c92a1d92a9658f6
shakib-007 <shakibur.rahman.fahim@gmail.com>
2021-03-23 13:50:14 +0600

    profile

---
+++ FINAL_LAB_TASK_03_PHP/view/profile.php
@@ -0,0 +1,1 @@
+<?php 	require_once('../model/userModel.php');?><center>	<table border="1" cellpadding="5" cellspacing="0">		<tr><td colspan="2" align="CENTER">Profile</td></tr>		<tr><td>ID</td><td></tr>		<tr><td>NAME</td><td></td></tr>			<tr><td>USER TYPE</td><td></td></tr>		<tr><td colspan="2" align="right"><a href="home.html">Go Home</a></td></tr>	</table>			</center>
